0599a7d446f15d0b84b9d0208751983d4ec397e3
larkin <88820972+larkin1@users.noreply.github.com>
2026-06-01 13:38:33 +1000

    changed wallpaper logic

--- scripts/wallpaper-cycle.sh
+++ scripts/wallpaper-cycle.sh
@@ -15,15 +15,44 @@ mtr=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
 current_wall=$(cat "$STATE_FILE")
 
 if [ -z "$current_wall" ]; then
-  next_wall=$(echo "$walls" | head -n 1)
+  case $1 in
+    next)
+      next_wall=$(echo "$walls" | head -n 1)
+      ;;
+    prev)
+      next_wall=$(echo "$walls" | tail -n 1)
+      ;;
+    *)
+      echo "Please specify an action (next/prev)"
+      exit 1
+      ;;
+  esac
 else
-  next_wall=$(echo "$walls" | grep -F -x "$current_wall" -A 1 | sed -n '2p')
+  case $1 in
+    next)
+      next_wall=$(echo "$walls" | grep -F -x "$current_wall" -A 1 | sed -n '2p')
+      ;;
+    prev)
+      next_wall=$(echo "$walls" | grep -F -x "$current_wall" -B 1 | sed -n '1p')
+      ;;
+    *)
+      echo "Please specify an action (next/prev)"
+      exit 1
+      ;;
+  esac
 fi
 
 echo "$next_wall"
 
 if [ -z "$next_wall" ]; then
-  next_wall=$(echo "$walls" | head -n 1)
+  case $1 in
+    prev)
+      next_wall=$(echo "$walls" | tail -n 1)
+      ;;
+    next)
+      next_wall=$(echo "$walls" | head -n 1)
+      ;;
+  esac
 fi 
 
 echo "$next_wall" > "$STATE_FILE"
